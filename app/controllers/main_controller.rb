4f426f937985548dc52d740158a3fd207801eee1
Derek Maly <dmaly18@gmail.com>
2023-11-27 15:56:54 -0800

    finished params

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,42 @@
 class MainController < ApplicationController
-    def landing
-        render html: 'Welcome to the landing page'
+#     def landing
+#         render html: 'Welcome to the landing page'
+#     end
+
+#     def members
+#         @members_names = ['Derek', 'Jeffrey']
+#     end
+
+# end
+
+    def cubed
+        @number = params[:number].to_i
+  @cubed_number = @number**3
     end
 
-    def members
-        @members_names = ['Derek', 'Jeffrey']
-    end
+  def check
+  @num1 = params[:num1].to_i
+  @num2 = params[:num2].to_i
+@evenly_divisible = (@num1 % @num2).zero?
+        # if num2.zero?
+        #     "Cannot divide by zero."
+        # elsif num1 % num2 == 0
+        #     "#{num1} is evenly divisible by #{num2}."
+        # else
+        #     "#{num1} is not evenly divisible by #{num2}."
+        # end
+  end
 
+  def palindrome
+    @word = params[:word]
+    @reversed_word = @word.downcase == @word.downcase.reverse
+  end
+
+  def madlib
+  @noun = params[:noun]
+  @verb = params[:verb]
+  @adjective = params[:adjective]
+  @adverb = params[:adverb]
+
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root 'main#landing'
-  get '/landing' => 'main#landing'
-  get '/members' => 'main#members'
+  # root 'main#landing'
+  # get '/landing' => 'main#landing'
+  # get '/members' => 'main#members'
+  root 'main#cubed'
+  get '/cubed/:number' => 'main#cubed'
+  get '/evenly/:num1/:num2' => 'main#check'
+  get '/palindrome/:word' => 'main#palindrome'
+  get '/madlib/:noun/:verb/:adjective/:adverb' => 'main#madlib'
 end
